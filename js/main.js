0787634041c36c6ed537e495b571ec0f043abff1
Mcaleese <kevin.mcaleese@libertymutual.com>
2017-08-30 16:40:28 -0400

    unit 8 added cards

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
+/* Flipping Queen 1 */
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log ("User flipped queen");
+
+
+/* Flipping King 1  */
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log ("User flipped king");
+
+
+/* ensuring 2 cards chosen */
+
 
