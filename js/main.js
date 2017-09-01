756eb6bf9d2f2709ecab79769897324d49d8908e
Mcaleese <kevin.mcaleese@libertymutual.com>
2017-09-01 15:03:30 -0400

    unit 10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+	{rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+	{rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+	{rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
+
 var cardsInPlay = [];
 
 /* function to check for match */
@@ -12,14 +31,19 @@ var checkForMatch = function () {
 /* function storing what happens when user flips card*/
 
 var flipCard = function (cardId) {
-	console.log ("User flipped " + cards[cardId]);
-	cardsInPlay.push (cards[cardId]);
-	if(cardsInPlay.length <2 || cardsInPlay.length > 2) {return};
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log (cards[cardId].cardImage);
+	console.log (cards[cardId].suit);
+	cardsInPlay.push (cards[cardId].rank);
+	if(cardsInPlay.length <2 || cardsInPlay.length > 2) return;
 	checkForMatch();
 };
 
 flipCard (0);
 flipCard (2);
+
+
+
 
 
 
