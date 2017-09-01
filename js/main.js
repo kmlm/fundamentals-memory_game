3ef7a828f0cfb9c7d1198855a4d435deeb8cb84e
Mcaleese <kevin.mcaleese@libertymutual.com>
2017-09-01 12:38:31 -0400

    unit 9 x1

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-console.log("Up and running!");
-
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
+/* function to check for match */
 
-/* Flipping Queen 1 */
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ("User flipped queen");
-
-
-/* Flipping King 1  */
-
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log ("User flipped king");
-
-
-/* ensuring 2 cards chosen */
-
-
-
-/* alerts for cards chosen */
-
-if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) 
+		{alert("You found a match!");} 
 	else {alert("Sorry, try again.");};
+};
+
+/* function storing what happens when user flips card*/
+
+var flipCard = function (cardId) {
+	console.log ("User flipped " + cards[cardId]);
+	cardsInPlay.push (cards[cardId]);
+	checkForMatch();
+};
+
+flipCard (0);
+flipCard (2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
